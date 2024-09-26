9d138aecb7443f5ba9afd2718048302eb51bd93a
Phuc-Duy <phucduy2501@gmail.com>
2024-09-27 01:02:36 +0700

    first commit

---
+++ assets/js/script.js
@@ -0,0 +1,6 @@
+// {/* <script>
+//    document.addEventListener("DOMContentLoaded", function() {
+//      let text = document.querySelector('.reveal-text');
+//      text.innerHTML = text.textContent.replace(/\S/g, "<span>$&</span>");
+//    });
+// </script> */}
